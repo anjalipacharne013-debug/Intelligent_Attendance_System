5a593be5fe1cb0ebfb4042a8c3766eaf458b4ba6
anjalipacharne013-debug <anjalipacharne013@gmail.com>
2026-07-27 00:11:44 +0530

    Fix indentation for main function in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.screens.home_screen import home_screen
 from src.screens.student_screen import student_screen
 from src.screens.teacher_screen import teacher_screen
 def main():
-     st.set_page_config(page_title="SnapClass - Attendance System using AI", page_icon="https://i.ibb.co/YTYGn5qV/logo.png")
+    st.set_page_config(page_title="SnapClass - Attendance System using AI", page_icon="https://i.ibb.co/YTYGn5qV/logo.png")
     if 'login_type' not in st.session_state:
         st.session_state['login_type'] = None
     
